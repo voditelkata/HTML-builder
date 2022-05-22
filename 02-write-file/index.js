c3ee91c6a4940f47444657659f8165e7ffca6e9c
voditelkatka <voditelkatka@gmail.com>
2022-05-22 09:57:07 +0300

    fix: add bye message when pressed ctrl+c

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,15 +1,17 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 const { stdin, stdout } = process;
 
-const way = path.join(__dirname, "write.txt");
+const way = path.join(__dirname, 'write.txt');
 const textFile = fs.createWriteStream(way);
-stdout.write("Please, enter text\n");
-stdin.on("data", (data) => {
-  if (data.toString().trim() === "exit") {
-    console.log("Good bye!");
+stdout.write('Please, enter text\n');
+stdin.on('data', (data) => {
+  if (data.toString().trim() === 'exit') {
+    console.log('Good bye!');
     process.exit();
   } else textFile.write(data);
 });
-
-// process.on("beforeExit", () => console.log("Good bye"));
+process.on('SIGINT', () => {
+  stdout.write('Bye!');
+  process.exit();
+});

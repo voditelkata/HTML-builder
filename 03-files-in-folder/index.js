1430bf2b050bc9d3a16c19c11b0009060994efdb
voditelkatka <voditelkatka@gmail.com>
2022-05-22 20:21:01 +0300

    fix: add async function for getting size of file

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,21 +1,20 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const wayDir = path.join(__dirname, 'secret-folder');
+const wayDir = path.join(__dirname, "secret-folder");
 
-fs.readdir(wayDir, { withFileTypes: true }, (err, data) => {
-  if (err) return console.error(err.message);
-  for (let item of data) {
-    if (item.isFile()) {
-      const wayFile = path.join(wayDir, item.name);
-      const name = path.parse(wayFile).name;
-      const extension = path.extname(wayFile).slice(1);
-      fs.stat(wayFile, (err, stats) => {
-        if (err) return console.error(err.message);
-        console.log(
-          `${name} - ${extension} - ${Math.floor(stats.size / 1024)}kb`
-        );
-      });
+fs.readdir(wayDir, { withFileTypes: true }, async (err, data) => {
+  try {
+    for (let item of data) {
+      if (item.isFile()) {
+        const wayFile = path.join(wayDir, item.name);
+        const name = path.parse(wayFile).name;
+        const extension = path.extname(wayFile).slice(1);
+        const size = await fs.promises.stat(wayFile);
+        console.log(`${name} - ${extension} - ${size.size}b`);
+      }
     }
+  } catch (err) {
+    console.error(err);
   }
 });
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,11 @@
+const fs = require("fs");
+const path = require("path");
 
+const fromDir = path.join(__dirname, 'files');
+const toDir = path.join(__dirname, 'files-copy');
+
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, err => {
+    if (err) throw err;
+});
+
+console.log(toDir)

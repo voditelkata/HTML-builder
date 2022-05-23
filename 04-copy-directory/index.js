107f60bcec1e4413577c9ee9dbcb958489232884
voditelkatka <voditelkatka@gmail.com>
2022-05-23 13:04:41 +0300

    feat: add solution for 04 submodule

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,11 +1,32 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 const fromDir = path.join(__dirname, 'files');
 const toDir = path.join(__dirname, 'files-copy');
 
-fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, err => {
-    if (err) throw err;
+fs.mkdir(toDir, { recursive: true }, (err) => {
+  if (err) throw err;
 });
 
-console.log(toDir)
+fs.readdir(toDir, (err, data) => {
+  if (err) throw err;
+  if (data.length > 0) {
+    for (let item of data) {
+      const wayToFile = path.join(toDir, item);
+      fs.unlink(wayToFile, (err) => {
+        if (err) throw err;
+      });
+    }
+  }
+  fs.readdir(fromDir, (err, data) => {
+    if (err) throw err;
+    for (let item of data) {
+      const wayFromFile = path.join(fromDir, item);
+      const wayToFile = path.join(toDir, item);
+      fs.copyFile(wayFromFile, wayToFile, (err) => {
+        if (err) throw err;
+        console.log(`${wayFromFile} was copied to ${wayToFile}`);
+      });
+    }
+  });
+});
